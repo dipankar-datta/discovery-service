3abba557374ab22d4dcd375f1cc18478d9ddfd12
dipankar-datta <dattapro5@gmail.com>
2019-07-08 14:25:29 +0530

    Renamed start up class name.

--- src/test/java/com/example/DiscoveryServiceTests.java
+++ src/test/java/com/example/DiscoveryServiceTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StartApplicationTests {
+public class DiscoveryServiceTests {
 
 	@Test
 	public void contextLoads() {
